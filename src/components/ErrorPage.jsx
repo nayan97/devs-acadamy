808007cb809d55ed8f8ac138e0cfab4c2d339d31
nayan97 <nayanislam085@gmail.com>
2025-06-12 17:05:41 +0600

    successfuly added error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import error from '../assets/error/error-page.jpg';
+import { Link } from 'react-router';
 
-const ErrorPage = () => {
+const links = <>
+        
+  <Link to="/"> <button className='m-2 btn btn-success text-white rounded-3xl'>Go Back Home Page</button></Link>
+
+</>
+
+const Error = () => {
     return (
-        <div>
-            
+        <div className='pt-20 mt-20'>
+         
+            <div className="flex justify-center mx-auto py-2">
+                <img src={error} alt="" />
+            </div>
+            <div className="flex justify-center mx-auto ">
+            {
+                 links
+             }
+            </div>
         </div>
     );
 };
 
-export default ErrorPage;
+export default Error;

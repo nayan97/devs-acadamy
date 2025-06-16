2c1f588849fadbce97920556d6dde2d28e8fb2ab
nayan97 <nayanislam085@gmail.com>
2025-06-16 15:52:48 +0600

    applyed search option

--- src/pages/Assignment/ViewAssignment.jsx
+++ src/pages/Assignment/ViewAssignment.jsx
@@ -6,11 +6,20 @@ const ViewAssignment = () => {
   const allAssignments = useLoaderData();
   const [assignments, setAssignments] = useState(allAssignments);
   const [selectedLevel, setSelectedLevel] = useState("All");
+  const [searchText, setSearchText] = useState("");
+
+  const filteredAssignments = assignments
+    .filter((assignment) =>
+      selectedLevel === "All" ? true : assignment.difficulty === selectedLevel
+    )
+    .filter((assignment) =>
+      assignment.title.toLowerCase().includes(searchText.toLowerCase())
+    );
   // console.log(assignments);
 
   return (
-    <div className="">
-      <div className="text-center">
+    <div className="min-h-screen">
+      <div className="flex justify-around p-4">
         <label className="input">
           <svg
             className="h-[1em] opacity-50"
@@ -28,7 +37,14 @@ const ViewAssignment = () => {
               <path d="m21 21-4.3-4.3"></path>
             </g>
           </svg>
-          <input type="search" className="grow" placeholder="Search" />
+
+          <input
+            type="search"
+            className="grow"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            placeholder="Search"
+          />
         </label>
         <select
           value={selectedLevel}
@@ -36,26 +52,20 @@ const ViewAssignment = () => {
           className="select select-bordered mb-4"
         >
           <option value="All">All</option>
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
         </select>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 p-6">
-        {assignments
-          .filter((assignment) =>
-            selectedLevel === "All"
-              ? true
-              : assignment.difficulty === selectedLevel
-          )
-          .map((assignment) => (
-            <AssignmentCard
-              key={assignment._id}
-              assignments={assignments}
-              setAssignments={setAssignments}
-              assignment={assignment}
-            />
-          ))}
+      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6 p-6 border border-amber-100 rounded-2xl">
+        {filteredAssignments.map((assignment) => (
+          <AssignmentCard
+            key={assignment._id}
+            assignments={assignments}
+            setAssignments={setAssignments}
+            assignment={assignment}
+          />
+        ))}
       </div>
     </div>
   );
